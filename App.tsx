4aa95668e381ed4ed7781476b5701f652950387f
yeasin-hossain <yeasinxdp@gmail.com>
2024-02-19 00:11:54 +0600

    INIT the env

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, useColorScheme, View} from 'react-native';
+import {useColorScheme, View} from 'react-native';
 import {Button} from '@rneui/themed';
 import {Icon} from '@rneui/themed';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
@@ -10,17 +10,14 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 function HomeScreen() {
   const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={{alignItems: 'center', justifyContent: 'center'}}>
-      <View
-        style={{
-          backgroundColor: isDarkMode ? Colors.black : Colors.white,
-        }}>
-        <Text>Hello</Text>
-        <Button title="Solid" />
-        <Button color="error">Error</Button>
-        <Icon reverse name="youtube" type="antdesign" color="#517fa4" />
-        <SmallButton title={'Yeasin'} size="md" color="success" />
-      </View>
+    <View
+      style={{
+        backgroundColor: isDarkMode ? Colors.black : Colors.white,
+      }}>
+      <Button title="Solid" />
+      <Button color="error">Error</Button>
+      <Icon reverse name="youtube" type="antdesign" color="#517fa4" />
+      <SmallButton title={'Yeasin'} size="md" color="success" />
     </View>
   );
 }
